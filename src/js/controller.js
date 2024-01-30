9a44519c751a6100ff3b7a8c72800daed2db4644
xinyufeifei <xinyufeifei@gmail.com>
2024-01-30 13:40:15 -0600

    add a log for continuous integration.

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,5 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('The continous integration was set up!');
 };
 init();
